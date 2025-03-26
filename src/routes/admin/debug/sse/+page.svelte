a11cd2b7f700281c33a4da51068a1557075518c1
Bernard Tan <140779864+bernardbuilder@users.noreply.github.com>
2025-03-26 16:55:36 +0800

    Refactored the menu system

---
+++ src/routes/admin/debug/sse/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+
+    import PageContainer from "$lib/components/ui_components_sveltekit/layout/PageContainer.svelte";
+    import PageHeader from "$lib/components/ui_components_sveltekit/layout/PageHeader.svelte";
+    
+    const pageCrumbs = [
+        ["Admin", "/admin"],
+        "Debug",
+        "SSE"
+    ];
+</script>
+<PageContainer crumbs={pageCrumbs}>
+    <PageHeader title="SSE">
+        <svelte:fragment slot="action">
+            <!-- <ActionButton
+                label="Add User"
+                icon={Plus}
+                onClick={() => goto('/admin/users/new')}
+            /> -->
+        </svelte:fragment>
+    </PageHeader>
+</PageContainer>
