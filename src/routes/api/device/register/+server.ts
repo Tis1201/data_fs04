bb9bb4183fadd397b80d60a8809d5cfcd62f9caa
Bernard Tan <140779864+bernardbuilder@users.noreply.github.com>
2025-05-05 13:41:40 +0800

    Add ping/heartbeat and cleanup logic to SSE connections

--- src/routes/api/device/register/+server.ts
+++ src/routes/api/device/register/+server.ts
@@ -81,11 +81,6 @@ export const GET: RequestHandler = async ({ params, locals, request }) => {
 
             DeviceManager.registerDevice(pin, deviceMeta);
 
-            const encoder = new TextEncoder();
-            controller.enqueue(encoder.encode("event: connected\ndata: {}\n\n"));
-
-
-
         },
 
         cancel() {
