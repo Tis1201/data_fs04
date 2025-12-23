16f5f9d61908d938bb9e007b2e0318d79781635b
Nguyen Huu Hoan <152596588+hoan-spectrio@users.noreply.github.com>
2025-12-23 08:44:02 +0700

    fix import

--- src/lib/server/mqtt/handlers/web/handle_sensor_config.ts
+++ src/lib/server/mqtt/handlers/web/handle_sensor_config.ts
@@ -1,6 +1,7 @@
 import type { RpcHandlerArgs, RpcResponse } from '$lib/server/mqtt/handlers/index';
 import { logger } from '$lib/server/logger';
-import { checkDeviceAccess } from './access_checker';
+import { checkDeviceAccess } from './shared/access_checker';
+import crypto from 'crypto';
 
 /**
  * Sensor Config Save Parameters
